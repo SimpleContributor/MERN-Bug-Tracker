5f8621d569724c5aa039d771c85732f6d86d3813
Caleb <calebcoecode@gmail.com>
2021-04-06 12:37:10 -0600

    Add two more tests. One tests POST User Login and the other tests GET User Profile.

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,6 +5,8 @@ const { userOneId, userOne, userTwoId, userTwo, setupDatabase } = require('./fix
 
 setupDatabase();
 
+let userToken;
+
 test('Should Signup a New User', async () => {
     const response = await request(app).post('/api/users').send({
         name: "Caleb Coe",
@@ -19,3 +21,24 @@ test('Should Signup a New User', async () => {
 });
 
 
+test('Should Login User', async () => {
+    const response = await request(app).post('/api/auth').set('Content-Type', 'application/json').send({
+        email: "calebcoe0@gmail.com",
+        password: "123456"
+    }).expect(200);
+
+    expect(response.body.token).not.toBeNull();
+
+    userToken = response.body.token;
+})
+
+
+test('Get User Profile Based on Users Token', async () => {
+    const response = await request(app)
+        .get('/api/profile/me')
+        .set('x-auth-token', userToken)
+        .send()
+        .expect(400);
+
+    expect(response.body.msg).toBe("There is no profile for this user...");
+})
