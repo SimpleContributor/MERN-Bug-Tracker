1dfb01387dbd412c169fc32896d204db5a7d7d69
Caleb <calebcoecode@gmail.com>
2021-04-21 23:18:17 -0600

    Add route GET /:project_id to get all of the tickets for a project.

--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -17,7 +17,7 @@ router.post('/:project_id/:ticket_id/comment', auth, async (req, res) => {
     const commentFields = {};
 
     commentFields.user = req.user.id;
-    commentFields.user.name = req.user.name;
+    commentFields.name = req.user.name;
     if (!comment) {
         // 418: The server refuses the attempt to brew coffee with a teapot
         return res.status(418).json({ msg: 'Need to enter a comment...'});
@@ -44,5 +44,17 @@ router.post('/:project_id/:ticket_id/comment', auth, async (req, res) => {
         res.status(500).send('Server Error...');
     }
 })
+
+router.get('/:project_id', auth, async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.project_id);
+        if (!project) return res.status(500).json({ msg: 'No project by this name found.' });
+
+        res.json(project.tickets);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error...');
+    };
+});
 
 module.exports = router;
