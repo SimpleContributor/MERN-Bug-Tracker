7817c645b0ac2ccfe71036da39e730ad38462c53
Caleb <calebcoecode@gmail.com>
2021-04-27 22:03:02 -0600

    Add route to create a ticket from /api/projects.js and change from a PUT route to POST.

--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -7,7 +7,52 @@ const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 
 
-// @route   PUT api/tickets/:ticket_id/comment
+
+// @route   POST api/tickets/:project_id
+// @desc    Create a Ticket
+// @access  Private
+router.post('/:project_id', auth, async (req, res) => {
+    const {
+        ticket,
+        severity,
+        status,
+    } = req.body;
+
+    const ticketFields = {
+        comments: []
+    };
+
+    if (ticket) ticketFields.ticket = ticket;
+    if (severity) ticketFields.severity = severity;
+    if (status) ticketFields.status = status;
+    ticketFields.user = req.user.id;
+    ticketFields.name = req.user.name;
+    //ticketFields._id = new mongoose.Types.ObjectId() //DOES NOT FIX err: Cast to ObjectId failed for value "ticket" at path "_id" for model "project"
+    //this error was caused by the model having an extra piece of data that a document in the database did not have. 
+
+    try {
+        const project = await Project.findById(req.params.project_id);
+        if (!project) return res.status(404).send('No project by this name found.');
+        console.log(project);
+
+        let validUser = await project.users.some(el => el.user.toString() === req.user.id);
+        if (!validUser) {
+            return res.status(404).send('User not Found to be a part of this project...');
+        }
+
+        project.tickets.push(ticketFields);
+        await project.save();
+
+        res.json(project.tickets);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error...');        
+    }
+})
+
+
+
+// @route   POST api/tickets/:project_id/:ticket_id/comment
 // @desc    Create a comment on a ticket
 // @access  Private
 router.post('/:project_id/:ticket_id/comment', auth, async (req, res) => {
@@ -45,7 +90,9 @@ router.post('/:project_id/:ticket_id/comment', auth, async (req, res) => {
     }
 })
 
-// @route   PUT api/tickets/:project_id
+
+
+// @route   GET api/tickets/:project_id
 // @desc    Get all tickets in a project
 // @access  Private
 router.get('/:project_id', auth, async (req, res) => {
@@ -68,7 +115,8 @@ router.get('/:project_id', auth, async (req, res) => {
     };
 });
 
-// @route   PUT api/tickets/:ticket_id
+
+// @route   GET api/tickets/:project_id/:ticket_id
 // @desc    Get a specific ticket
 // @access  Private
 router.get('/:project_id/:ticket_id', auth, async (req, res) => {
